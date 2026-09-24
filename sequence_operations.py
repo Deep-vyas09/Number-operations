3d6834a641f28446ab145eb95192b453ea28b5b6
Deepali Vyas <dvyas0911@gmail.com>
2026-09-24 18:55:43 +0530

    Update sequence_operations.py

--- sequence_operations.py
+++ sequence_operations.py
@@ -1,17 +1,18 @@
-def find_factors(n: int) -> list[int]:
-    """Returns a list of factors for a given number."""
-    if n <= 0:
-        return []
-    return [i for i in range(1, n + 1) if n % i == 0]
+def find_factors(n):
+    """Finds all factors of a number."""
+    factors = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            factors.append(i)
+    return factors
 
-
-def fibonacci(count: int) -> list[int]:
-    """Generates Fibonacci series up to specified count of numbers."""
-    if count <= 0:
-        return []
+def fibonacci(num):
+    """Generates fibonacci sequence up to 'num' terms."""
     a, b = 0, 1
     fib = []
-    for _ in range(count):
+    for _ in range(num):
         fib.append(a)
-        a, b = b, a + b
+        c = a + b
+        a = b
+        b = c
     return fib
